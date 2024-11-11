25853b4a75115963abb2a9bfb91a1cfa539d0c1d
Hyosang_An <gytharj@gmail.com>
2024-11-11 19:32:05 +0900

    258까지 완료

--- Project/Engine/CLandScape.cpp
+++ Project/Engine/CLandScape.cpp
@@ -10,6 +10,7 @@ CLandScape::CLandScape()
 	, m_FaceX(1)
 	, m_FaceZ(1)
 {
+	SetFrustumCheck(false);
 	SetFace(m_FaceX, m_FaceZ);
 }
 
@@ -25,14 +26,24 @@ void CLandScape::Render()
 {
 	Transform()->Binding();
 
-	GetMaterial()->GetShader()->SetRSType(RS_TYPE::WIRE_FRAME);
+	//GetMaterial()->GetShader()->SetRSType(RS_TYPE::WIRE_FRAME);
+
+	// 지형의 면 개수
+	GetMaterial()->SetScalarParam(INT_0, m_FaceX);
+	GetMaterial()->SetScalarParam(INT_1, m_FaceZ);
+	GetMaterial()->SetScalarParam(FLOAT_0, m_TessLevel);
+
+	// 지형에 적용시킬 높이맵
+	GetMaterial()->SetTexParam(TEX_0, m_HeightMap);
+
+	// 재질 바인딩
 	GetMaterial()->Binding();
 
-
+	// 렌더링
 	GetMesh()->Render();
 }
 
-void CLandScape::SetFace(UINT _X, UINT _Z)
+void CLandScape::SetFace(int _X, int _Z)
 {
 	m_FaceX = _X;
 	m_FaceZ = _Z;
